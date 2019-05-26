f755ab7ecf24a07bed4ffd13f91812d5528d91e7
Barnes7599 <barnes7599@gmail.com>
2019-05-26 06:20:15 -0400

    Finished

--- KNN Project3.R
+++ KNN Project3.R
@@ -25,3 +25,37 @@ head(final.df)
 sample <- sample.split(final.df$V2, SplitRatio = 0.7)
 sample.train <- subset(final.df, sample == TRUE)
 sample.test <- subset(final.df, sample == FALSE)
+
+set.seed(101)
+predicted.df <- knn(sample.train[1:32], sample.test[1:32], sample.train$V2, k = 8)
+
+error.rate  <-  mean(sample.test$V2 != predicted.df)
+
+error.rate
+
+sqrt(198)
+
+table(sample.test$V2, predicted.df)
+
+recall <- 2/(2+12)
+recall
+
+predicted.species <-  NULL
+error.rate <- NULL
+
+for (i in 1:10) {
+    set.seed(101)
+    predicted.df <- knn(sample.train[1:32], sample.test[1:32], sample.train$V2, k = i)
+    error.rate[i] <-  mean(sample.test$V2 != predicted.df)
+}
+
+
+k.values <- 1:10
+error.df <- data.frame(error.rate, k.values)
+
+ggplot(error.df, aes(k.values, error.rate)) + 
+    geom_point() +
+    geom_line(lty = 'dotted', color = 'red') +
+    theme_bw()
+
+
